ac10a51f8f02953b7c3ef33232dedba92d47f145
xlslucky <xlslucky@gmail.com>
2020-12-21 19:50:03 +0800

    init

---
+++ src/Test/Test.stories.tsx
@@ -0,0 +1,11 @@
+// Generated with util/create-component.js
+import React from "react"
+import Test from "./Test"
+
+export default {
+    title: "Test"
+}
+
+export const WithBar = () => <Test foo="bar" />
+
+export const WithBaz = () => <Test foo="baz" />
---
+++ src/Test/Test.tsx
@@ -0,0 +1,13 @@
+// Generated with util/create-component.js
+import React from "react"
+
+import { TestProps } from "./Test.types"
+
+import "./Test.scss"
+
+const Test: React.FC<TestProps> = ({ foo }) => (
+    <div data-testid="Test" className="foo-bar">{foo}</div>
+)
+
+export default Test
+
