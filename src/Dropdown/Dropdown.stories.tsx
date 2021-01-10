74391eaf23c8cd462beeb33f795f8275d8c0a4c8
xlslucky <xlslucky@gmail.com>
2021-01-10 15:11:11 +0800

    docs(Dropdown): update doc

--- src/Dropdown/Dropdown.stories.tsx
+++ src/Dropdown/Dropdown.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Dropdown from '.'
 
 import Button from '../Button'
+import Menu from '../Menu'
 
 import '../style/storybook.scss'
 
@@ -30,20 +31,39 @@ const BOTTOM_GROUP: GroupType = [
 ]
 
 export const Default = () => (
-  <div style={{ margin: '0 auto', width: 500, paddingTop: 50 }}>
-    <div style={{ marginLeft: 70, marginBottom: 10 }}>
-      {TOP_GROUP.map(({ label, ...item }, index) => (
-        <Dropdown disabled key={index} overlay={<span>{TITLE}</span>} {...item}>
-          <Button style={{ width: 70, marginRight: 15 }}>{label}</Button>
-        </Dropdown>
-      ))}
+  <div>
+    <div style={{ margin: '0 auto', width: 500, paddingTop: 50 }}>
+      <div style={{ marginLeft: 70, marginBottom: 10 }}>
+        {TOP_GROUP.map(({ label, ...item }, index) => (
+          <Dropdown
+            disabled
+            key={index}
+            overlay={<span>{TITLE}</span>}
+            {...item}
+          >
+            <Button style={{ width: 70, marginRight: 15 }}>{label}</Button>
+          </Dropdown>
+        ))}
+      </div>
+      <div style={{ marginLeft: 70, marginTop: 10 }}>
+        {BOTTOM_GROUP.map(({ label, ...item }, index) => (
+          <Dropdown key={index} overlay={<span>{TITLE}</span>} {...item}>
+            <Button style={{ width: 70, marginRight: 15 }}>{label}</Button>
+          </Dropdown>
+        ))}
+      </div>
     </div>
-    <div style={{ marginLeft: 70, marginTop: 10 }}>
-      {BOTTOM_GROUP.map(({ label, ...item }, index) => (
-        <Dropdown key={index} overlay={<span>{TITLE}</span>} {...item}>
-          <Button style={{ width: 70, marginRight: 15 }}>{label}</Button>
-        </Dropdown>
-      ))}
+    <div>
+      <Dropdown
+        overlay={
+          <Menu>
+            <Menu.Item key={1}>1</Menu.Item>
+            <Menu.Item key={2}>2</Menu.Item>
+          </Menu>
+        }
+      >
+        <Button>menu</Button>
+      </Dropdown>
     </div>
   </div>
 )
